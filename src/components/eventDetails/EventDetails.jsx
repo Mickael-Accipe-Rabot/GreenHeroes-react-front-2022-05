650494fdecbb855d2e0a92e613c9bc93ca2cbfba
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-05-12 22:55:23 +0200

    css for details and add players list and play button

--- src/components/eventDetails/EventDetails.jsx
+++ src/components/eventDetails/EventDetails.jsx
@@ -24,34 +24,62 @@ export const EventDetails = () => {
         <div className="details-map">map</div>
 
         <div className="details-items">
-
           <div className="details-top">
             <h1>{eventDetails.title}</h1>
-            </div>
-            <div className='details-top-info'>
-            <p>{eventDetails.type}</p>
+          </div>
+          <div className="details-top-info">
+            <p>Catégorie : {eventDetails.type}</p>
             <p>crée par {eventDetails.author}</p>
-            
-            
           </div>
           <hr></hr>
           <div className="details-bottom">
-          
             <p>{eventDetails.start_date}</p>
             <p>{eventDetails.start_hour}</p>
-            
-            <p>{eventDetails.street_number} {eventDetails.street_name}</p>
-            
-            <p>{eventDetails.city}, {eventDetails.zip_code}, {eventDetails.country}</p>
- 
-            
+
+            <div className="details-adress">
+              <p>
+                {eventDetails.street_number} {eventDetails.street_name}
+              </p>
+              <p className="details-city">
+                {eventDetails.city}, {eventDetails.zip_code},{' '}
+                {eventDetails.country}
+              </p>
+            </div>
+
             <hr></hr>
-            <p>description: </p>
-            <p>{eventDetails.description}</p>
-            {/* <p>{eventDetails.x_coor}</p>
+            <div className="details-description">
+              <p className="details-description-text">description : </p>
+              <p>{eventDetails.description}</p>
+              {/* <p>{eventDetails.x_coor}</p>
             <p>{eventDetails.y_coor}</p> */}
+            </div>
+            <hr></hr>
           </div>
         </div>
+        <button className="green-btn participer">Participer</button>
+
+        <hr className="hr-end"></hr>
+        <div className="players-number">
+          <span>nombre de participants :</span>
+          <span> 6{/*{eventDetails.players.length}*/}</span>
+        </div>
+        
+          {/* <div className="players-list-name">
+          {players.map((player) => (
+            <div className="player-item">
+              <p>{player.name}</p>
+            </div>
+          ))}
+        </div> */}
+        <div className="players-list-name">
+          <p className="player-item">john</p>
+          <p className="player-item">Koko</p>
+          <p className="player-item">Jojo</p>
+          <p className="player-item">Lolo</p>
+          <p className="player-item">Toto</p>
+          <p className="player-item">Soso</p>
+        </div>
+        {/* </div> */}
       </div>
     </div>
   );
