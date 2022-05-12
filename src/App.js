f4404b2ed6777971c84233401581d90b26ceb128
ACCIPE-RABOT MICKAEL <mickael.accipe.rabot@gmail.com>
2022-05-12 17:43:50 +0200

    create eventCard, eventDetails, eventCardsList for testing database and form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Informer } from './pages/informer/Informer';
 import { Agir } from './pages/agir/Agir';
 import { Home } from './pages/home/Home';
 import {axios} from 'axios';
+import { EventDetails } from './components/eventDetails/EventDetails';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/agir" element={ <Agir/>} />
         <Route path="/informer" element={<Informer/>} />
+        <Route path="/eventdetails/:id" element={<EventDetails/>} />
+        
 
       </Routes>
       </div>
