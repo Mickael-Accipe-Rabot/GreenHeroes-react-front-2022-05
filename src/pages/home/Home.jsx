681edab749c1367b15b28cd66b1494b22a71ce1c
Guillaume <miche.guillaume@isitparis.eu>
2022-05-13 13:38:34 +0200

    remove logo on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import './home.css';
-import { NavLink, Link } from 'react-router-dom';
+import React from "react";
+import "./home.css";
+import { NavLink, Link } from "react-router-dom";
 
 export const Home = () => {
   return (
-    <div className='body-home'>
+    <div className="body-home">
       <div className="home-container">
-        <div className='first-line'>
-        <h1 className="home-title"><span className="green">GREEN</span> <span className="heroes">HEROES</span></h1>
-        <Link to='login' className='login-button'>LOGIN</Link>
+        <div className="first-line">
+          <Link to="login" className="login-button">
+            LOGIN
+          </Link>
         </div>
         <div className="home-text">
           <h3 className="home-subtitle">
-            La nature à besoin de héros, les héros ont besoin de{' '}
+            La nature a besoin de héros, les héros ont besoin de{" "}
             <div className="greenHeroes">
               <span className="green">GREEN</span>
               <span className="heroes">HEROES</span>
-              
             </div>
           </h3>
 
           <ul className="choiceList">
             <li className="choice-item">
-            <NavLink to="/agir" className="choice-buttons-b">
+              <NavLink to="/agir" className="choice-buttons-b">
                 AGIR
               </NavLink>
             </li>
